3e967c45dc81039987ca55ff3d3959004bb94564
Cara <jiangyucara@gmail.com>
2020-06-29 15:22:53 -0400

    initial commit

---
+++ src/main/java/com/hypebabeus/myecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,29 @@
+package com.hypebabeus.myecommerce.config;
+
+import com.hypebabeus.myecommerce.entity.Product;
+import com.hypebabeus.myecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        // lock down product repository and product category repository to make them read-only
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // disable HTTP methods for PUT, POST, DELETE for product
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        // disable HTTP methods for PUT, POST, DELETE for product_category
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+    }
+}
